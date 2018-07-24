5b2b03d56e61b2cdaa034acb401963f63bf3398b
SoumenduKhilar <40198821+SoumenduKhilar@users.noreply.github.com>
2018-07-25 00:57:32 +0530

    Updated HelloWorld.java
    Added a new method welcome().

--- Samples/HelloWorld.java
+++ Samples/HelloWorld.java
@@ -4,10 +4,13 @@ public class HelloWorld {
 	void hello() {
 		System.out.println("Hello World!!!");
 	}
-
+        void welcome(){
+		System.out.println("Welcome");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HelloWorld obj = new HelloWorld();  //New object obj created for class HelloWorld
 		obj.hello();  //Object obj calls method hello()
+		obj.welcome();
 	}
 }
